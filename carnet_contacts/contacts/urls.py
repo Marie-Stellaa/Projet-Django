7146a576d2dad7a5164403f6c8f75ddebee0e3de
Gode Ayekpa Epiphanie Marie-Stella Rachel <ayekpa.gode@iit.ci>
2026-08-20 11:26:02 +0000

    creation de formulaire et du CRUD

--- carnet_contacts/contacts/urls.py
+++ carnet_contacts/contacts/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.liste_contacts, name='liste_contacts'),
+    path('ajouter/', views.creer_contact, name='creer_contact'),
+    path('modifier/<int:contact_id>/', views.modifier_contact, name='modifier_contact'),
+    path('supprimer/<int:contact_id>/', views.supprimer_contact, name='supprimer_contact'),
     path('<int:contact_id>/', views.detail_contact, name='detail_contact'),
+
 ]
