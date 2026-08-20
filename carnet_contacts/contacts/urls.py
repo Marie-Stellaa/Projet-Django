ea59f5f93267790c564aae01d3e8ae20edb1c486
Gode Ayekpa Epiphanie Marie-Stella Rachel <ayekpa.gode@iit.ci>
2026-08-20 16:06:25 +0000

    modification de tout le projet

--- carnet_contacts/contacts/urls.py
+++ carnet_contacts/contacts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.liste_contacts, name='liste_contacts'),
-    path('<int:pk>/', views.detail_contact, name='detail_contact'),
+    path('<int:pk>/', views.detail_contacts, name='detail_contacts'),
     path('ajouter/', views.ajouter_contact, name='ajouter_contact'),
     path('<int:pk>/modifier/', views.modifier_contact, name='modifier_contact'),
     path('<int:pk>/supprimer/', views.supprimer_contact, name='supprimer_contact'),
